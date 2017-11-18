4e1803ce4a73b549d04723f13281c6613928e94f
PocketTaCo <wizardrobe8@gmail.com>
2017-11-17 14:18:50 -1000

    calculator

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 var output;
 function calculator(x, y, z) {
-	
+	if(x=="+") {
+		output = y+z;
+	}else if(x=="-"){
+		output = y-z
+	}else if(x=="*"){
+		output = y*z
+	}else if(x=="/"){
+		output = y/z
+	}
 }
+
+calculator("+", 45, 100);
+calculator("-", 45, 100);
+calculator("*", 45, 100);
+calculator("/", 45, 100);
+
+console.log(output);
